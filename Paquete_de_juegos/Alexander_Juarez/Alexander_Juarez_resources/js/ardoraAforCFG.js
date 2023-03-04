c3b49bcb0db8c028c907ce62e1f034a00f25b9f8
AlexanderAntonioJuarez <126708114+AlexanderAntonioJuarez@users.noreply.github.com>
2023-03-03 20:16:13 -0800

    Paquete de juegos
    paquete de juegos ardora grupo 4

---
+++ Paquete_de_juegos/Alexander_Juarez/Alexander_Juarez_resources/js/ardoraAforCFG.js
@@ -0,0 +1,34 @@
+//Creado con Ardora - www.webardora.net
+//bajo licencia Attribution-NonCommercial-NoDerivatives 4.0 International (CC BY-NC-ND 4.0)
+//para otros usos contacte con el autor
+var timeAct=360; timeIni=360; timeBon=0;
+var successes=0; successesMax=5; attempts=0; attemptsMax=5;
+var score=0; scoreMax=5; scoreInc=1; scoreDec=0;
+var typeGame=0;
+var tiTime=false;
+var tiTimeType=1;
+var tiButtonTime=true;
+var textButtonTime="Comenzar";
+var tiSuccesses=false;
+var tiAttempts=true;
+var tiScore=false;
+var startTime;
+var colorBack="#FFFDFD"; colorButton="#91962F"; colorText="#000000"; colorSele="#FF8000";
+var goURLNext=false; goURLRepeat=false;tiAval=false;
+var scoOk=0; scoWrong=0; scoOkDo=0; scoWrongDo=0; scoMessage=""; scoPtos=10;
+var fMenssage="Verdana, Geneva, sans-serif";
+var fActi="Verdana, Geneva, sans-serif";
+var fEnun="Verdana, Geneva, sans-serif";
+var timeOnMessage=5; messageOk="FELICIDADES"; messageTime="SE ACABO EL TIEMPO"; messageError="MAL HECHO "; messageErrorG="MAL HECHO "; messageAttempts="FALLASTE"; isShowMessage=false;
+var urlOk=""; urlTime=""; urlError=""; urlAttempts="";
+var goURLOk="_blank"; goURLTime="_blank"; goURLAttempts="_blank"; goURLError="_blank"; 
+borderOk="#008000"; borderTime="#FF0000";borderError="#FF0000"; borderAttempts="#FF0000";
+var wordsGame="QWxleGFuZGVyX0p1YXJleg=="; wordsStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+function giveZindex(typeElement){var valueZindex=0; capas=document.getElementsByTagName(typeElement);
+for (i=0;i<capas.length;i++){if (parseInt($(capas[i]).css("z-index"),10)>valueZindex){valueZindex=parseInt($(capas[i]).css("z-index"),10);}}return valueZindex;}
+var words=["UkVHQUxBRE8=","T0xJR0FSUVVJQQ==","QVJBVUpP","Q0FGRQ==","TElCRVJBTA==","","","","",""];imaW=["","","","","","","","","",""];queW=["General y expresidente de la republica de El Salvador","Forma de gobernar en donde solo unos ostentan el poder","Expresidente el cual fe asesinado a machetazos y dos tiros","Grano con el cual El Salvador aumento sus exportaciones","Ideologia la cual cree que el mercado se establiza por si mismo","","","","",""];var wordsL=[8,10,6,4,7,"","","","",""];var altW=["","","","","","","","","",""];
+var auW=["","","","","","","","","",""];
+var fillLetter="ABCDEFGHIJKLMNÑOPQRSTUVWXYZÁÉÍÓÚÜ "; wordsG=[];var wordsGL=[]; indexG=0; actMaxWidth=550; indexWord=0; totalWord=0;
+var alterW=[];var imageW=[];questionW=[];audioW=[];profG=0; dirMedia="Alexander_Juarez_resources/media/"; textBNext="";
+colorText="#000000";colorButton="#0099FF";colorBack="#FFFFFF";colorSele="#DEEDFE";
+fMenssage="Verdana, Geneva, sans-serif";fActi="Verdana, Geneva, sans-serif";fEnun="Verdana, Geneva, sans-serif";
